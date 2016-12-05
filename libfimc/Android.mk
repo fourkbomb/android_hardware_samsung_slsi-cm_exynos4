70e8a97bf736a347b76f0fa363822a570bd563c0
Simon Shields <keepcalm444@gmail.com>
2016-12-05 22:45:57 +1100

    libexynosfimc: cleanup, build for all exynos4
    Change-Id: I3a4081810033950adada31eedc989cf0eedd8426

--- libfimc/Android.mk
+++ libfimc/Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(filter-out exynos4412,$(TARGET_BOARD_PLATFORM)),)
+ifeq ($(filter-out exynos4,$(TARGET_BOARD_PLATFORM)),)
 
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
--- libfimc/exynos_fimc.c
+++ libfimc/exynos_fimc.c
@@ -50,27 +50,27 @@ static int exynos_fimc_m2m_stop(void *handle);
 
 int HAL_PIXEL_FORMAT_2_V4L2_PIX(
     int hal_pixel_format)
-{   
+{
     int v4l2_pixel_format = -1;
-    
+
     switch (hal_pixel_format) {
     case HAL_PIXEL_FORMAT_RGBA_8888:
     case HAL_PIXEL_FORMAT_RGBX_8888:
         v4l2_pixel_format = V4L2_PIX_FMT_RGB32;
         break;
-    
+
     case HAL_PIXEL_FORMAT_RGB_888:
         v4l2_pixel_format = V4L2_PIX_FMT_RGB24;
         break;
-    
+
     case HAL_PIXEL_FORMAT_RGB_565:
         v4l2_pixel_format = V4L2_PIX_FMT_RGB565;
         break;
-        
+
     case HAL_PIXEL_FORMAT_BGRA_8888:
         v4l2_pixel_format = V4L2_PIX_FMT_BGR32;
         break;
-    
+
     case HAL_PIXEL_FORMAT_RGBA_5551:
         v4l2_pixel_format = V4L2_PIX_FMT_RGB555X;
         break;
