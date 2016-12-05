2187f947ca98665ad361a97a89861fb22aad902f
Simon Shields <keepcalm444@gmail.com>
2016-12-05 20:57:39 +1100

    exynos4: remove unused liblights
    Change-Id: Ibc98beb87973ed970fc88c6827b4784b88b55a8b

--- smdk_common/Android.mk
+++ smdk_common/Android.mk
@@ -14,6 +14,6 @@
 # limitations under the License.
 #
 
-common_smdk_dirs := libaudio libaudiohw liblight keychars libcsc
+common_smdk_dirs := libaudio libaudiohw keychars libcsc
 
 include $(call all-named-subdir-makefiles,$(common_smdk_dirs))
