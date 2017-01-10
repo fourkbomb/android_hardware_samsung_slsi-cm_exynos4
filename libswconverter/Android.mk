5ddad6c403d51547e5724a8f7946cc2570ed0f65
Simon Shields <keepcalm444@gmail.com>
2017-01-11 00:14:19 +1100

    exynos4: fix libswconverter build
    Change-Id: If3d1ea042220140697a494954f7da5509f165219

--- libswconverter/Android.mk
+++ libswconverter/Android.mk
@@ -6,6 +6,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
 	swconvertor.c \
+	csc_ARGB8888_to_YUV420SP_NEON.s \
 	csc_linear_to_tiled_crop_neon.s \
 	csc_linear_to_tiled_interleave_crop_neon.s \
 	csc_tiled_to_linear_crop_neon.s \
@@ -15,8 +16,8 @@ LOCAL_SRC_FILES := \
 LOCAL_C_INCLUDES := \
 	$(TOP)/$(TARGET_OMX_PATH)/include/khronos \
 	$(TOP)/$(TARGET_OMX_PATH)/include/sec \
-	$(TOP)/$(TARGET_HAL_PATH)/include \
-	$(TOP)/$(TARGET_HAL_PATH)/libhwconverter
+	$(LOCAL_PATH)/../include \
+	$(LOCAL_PATH)/../libhwconverter
 
 ifeq ($(BOARD_USE_SAMSUNG_COLORFORMAT), true)
 LOCAL_CFLAGS += -DUSE_SAMSUNG_COLORFORMAT
